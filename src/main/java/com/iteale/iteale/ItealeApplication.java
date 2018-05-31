e53930bf93a5bcfe0dd5ab4e0fafc1709bb6900d
JunWang <1103870790@qq.com>
2018-05-31 15:08:58 +0800

    Initial commit

---
+++ src/main/java/com/iteale/iteale/ItealeApplication.java
@@ -0,0 +1,12 @@
+package com.iteale.iteale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItealeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItealeApplication.class, args);
+	}
+}
