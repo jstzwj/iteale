6334b4e814095db69eb8208a4295779db3bb6c7f
JunWang <1103870790@qq.com>
2018-06-01 02:16:21 +0800

    add some resources

--- src/main/java/com/iteale/iteale/controller/UserController.java
+++ src/main/java/com/iteale/iteale/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.iteale.iteale.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class UserController {
 	@RequestMapping("/user")
     public ModelAndView showUser(){
