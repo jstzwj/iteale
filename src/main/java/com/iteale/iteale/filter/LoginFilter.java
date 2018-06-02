35251fcfd2156cfb22727aee5b4e63a2ccc7e75d
JunWang <1103870790@qq.com>
2018-06-02 16:35:37 +0800

    add filter

---
+++ src/main/java/com/iteale/iteale/filter/LoginFilter.java
@@ -0,0 +1,43 @@
+package com.iteale.iteale.filter;
+
+import java.io.IOException;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.iteale.iteale.User;
+
+@WebFilter(urlPatterns={"/setting","/user", "/post"})
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpSession session = ((HttpServletRequest)request).getSession();
+		User user = (User)session.getAttribute("user");
+		if(user == null)
+		{
+			((HttpServletResponse)response).sendRedirect("login");
+		}
+		chain.doFilter(request, response);
+		
+	}
+}  
